e7b3df7127ea4e7b9f4d2302155a424c840c3f85
ixTec <christian.dalby@datacamp.com>
2022-08-30 13:39:55 +0100

    feat: add new `useTitleCase` custom hook helper

---
+++ src/hooks/use-title-case.ts
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+import { TITLE_CASE_STOP_WORDS } from './config/title-case-stops';
+
+function useTitleCase(content: string) {
+  const [titleCasedContent, setTitleCasedContent] = useState<string>();
+
+  useEffect(() => {
+    //
+    setTitleCasedContent(
+      content
+        .toLowerCase()
+        .split(' ')
+        .reduce((output, segment, index) => {
+          // Always capitalise the first letter of the first word, or if it's not included in our stop words
+          if (TITLE_CASE_STOP_WORDS.includes(segment) && index) {
+            return output.concat(' ', segment);
+          } else {
+            return output.concat(
+              ' ',
+              segment[0].toUpperCase(),
+              segment.substring(1),
+            );
+          }
+        }, ''),
+    );
+  }, [content]);
+
+  return titleCasedContent;
+}
+export default useTitleCase;
