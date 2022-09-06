8ee082e250c25513c2f45d90f24f1f6668f2680c
Mateusz Łachacz <info@blurbyte.com>
2022-09-06 15:54:22 +0200

    fix(icon): add missing currentColor to LinkedInBrand

--- src/icon/generated/linked-in-brand.tsx
+++ src/icon/generated/linked-in-brand.tsx
@@ -7,7 +7,10 @@ type LinkedInBrandProps = Omit<React.ComponentProps<typeof Icon>, 'children'>;
 function LinkedInBrand({ size, ...restProps }: LinkedInBrandProps) {
   return (
     <Icon viewBox="0 0 18 18" size={size} {...restProps}>
-      <path d="M15.3 15.4h-2.7v-4.2c0-1 0-2.3-1.4-2.3S9.6 10 9.6 11.1v4.2H7V6.8h2.6V8c.5-.9 1.5-1.4 2.5-1.4 2.7 0 3.2 1.8 3.2 4.1v4.7zM4 5.6c-.8 0-1.5-.7-1.5-1.5S3.2 2.5 4 2.5s1.5.7 1.5 1.5c.1.9-.6 1.6-1.5 1.6zm1.3 9.8H2.7V6.8h2.7v8.6zM16.7.1H1.3C.6 0 0 .6 0 1.4v15.4c0 .7.6 1.3 1.3 1.3h15.3c.7 0 1.3-.6 1.3-1.3V1.4C18 .6 17.4 0 16.7.1z" />
+      <path
+        fill="currentColor"
+        d="M15.3 15.4h-2.7v-4.2c0-1 0-2.3-1.4-2.3S9.6 10 9.6 11.1v4.2H7V6.8h2.6V8c.5-.9 1.5-1.4 2.5-1.4 2.7 0 3.2 1.8 3.2 4.1v4.7zM4 5.6c-.8 0-1.5-.7-1.5-1.5S3.2 2.5 4 2.5s1.5.7 1.5 1.5c.1.9-.6 1.6-1.5 1.6zm1.3 9.8H2.7V6.8h2.7v8.6zM16.7.1H1.3C.6 0 0 .6 0 1.4v15.4c0 .7.6 1.3 1.3 1.3h15.3c.7 0 1.3-.6 1.3-1.3V1.4C18 .6 17.4 0 16.7.1z"
+      />
     </Icon>
   );
 }
