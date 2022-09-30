6dcad2207e416fbcb2d016eb3529428042e539f0
ixTec <christian.dalby@datacamp.com>
2022-09-30 14:31:52 +0100

    docs(button): update casing of component name

--- src/button/button-internal.tsx
+++ src/button/button-internal.tsx
@@ -10,28 +10,28 @@ import ButtonLoader from './loader';
 import type { PolymorphicRef, PolymorphicComponentProps } from '../helpers';
 
 type ButtonBaseProps = {
-  /* Defines the variant of the button. */
+  /* Defines the variant of the Button. */
   /* @default primary */
   variant?: 'primary' | 'secondary' | 'plain' | 'destructive' | 'upgrade';
-  /* Defines the size of the button. In most cases, the default size should be used. */
+  /* Defines the size of the Button. In most cases, the default size should be used. */
   /* @default medium */
   size?: 'small' | 'medium' | 'large';
-  /* Whether the button is in it's loading state. The button will maintain the minimum width of it's child content. */
+  /* Whether the Button is in it's loading state. The Button will maintain the minimum width of it's child content. */
   /* @default false */
   isLoading?: boolean;
   /* Allows the button to grow to the width of its container. */
   /* @default false */
   fullWidth?: boolean;
-  /* Sets the style of the button suitable for dark backgrounds. */
+  /* Sets the style of the Button suitable for dark backgrounds. */
   /* @default false */
   inverted?: boolean;
-  /* Disables the default _Title Casing_ when text button content is provided. Use only in rare situations. */
+  /* Disables the default _Title Casing_ when text Button content is provided. Use only in rare situations. */
   /* @default false */
   disableTitleCase?: boolean;
 };
 
 type ButtonIconOnlyProps = {
-  /* An icon displayed as the only content of the button. Because of that `aria-label` attribute must be specified. Could be any [Icon](/components/icon) from Waffles (use default size) or a custom component. */
+  /* An icon displayed as the only content of the Button. Because of that `aria-label` attribute must be specified. Could be any [Icon](/components/icon) from Waffles (use default size) or a custom component. */
   icon: JSX.Element;
   children?: never;
   iconLeft?: never;
