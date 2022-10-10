4eabe9010e5eeb66dd5877954307ac63cf11fba2
blurbyte <info@blurbyte.com>
2022-10-10 16:45:37 +0200

    feat(table): show shadows to the sides if table is scrollable

--- src/table/styles.ts
+++ src/table/styles.ts
@@ -3,29 +3,118 @@ import { css } from '@emotion/react';
 import { tokens } from '../tokens';
 import { hexToRgba } from '../helpers';
 
-type TableWrapperStyleOptions = {
-  isFocusVisible: boolean;
+// Explicit height for head cells must be set to offset shadows from top
+const HEAD_CELL_HEIGHT = 48;
+
+// Simple wrapper required for shadows to render properly
+export function outerWrapperStyle() {
+  return css`
+    position: relative;
+  `;
+}
+
+type WithShadows = {
+  hasShadowLeft: boolean;
+  hasShadowRight: boolean;
 };
 
+type TableWrapperStyleOptions = {
+  isFocusVisible: boolean;
+} & WithShadows;
+
+// When shadows are visible corners are not rounded
 export function tableWrapperStyle({
   isFocusVisible,
+  hasShadowLeft,
+  hasShadowRight,
 }: TableWrapperStyleOptions) {
   return css`
     outline: 0;
-    border-radius: ${tokens.borderRadius.medium};
     overflow: auto;
     transition: box-shadow 125ms ease-out;
+
+    ${!hasShadowLeft &&
+    css`
+      border-top-left-radius: ${tokens.borderRadius.medium};
+      border-bottom-left-radius: ${tokens.borderRadius.medium};
+    `}
+
+    ${!hasShadowRight &&
+    css`
+      border-top-right-radius: ${tokens.borderRadius.medium};
+      border-bottom-right-radius: ${tokens.borderRadius.medium};
+    `}
 
     ${isFocusVisible && `box-shadow: 0 0 0 2px ${tokens.colors.blueDark};`}
   `;
 }
 
-export function tableStyle() {
+type ShadowsStyleOptions = WithShadows;
+
+export function shadowsStyle({
+  hasShadowLeft,
+  hasShadowRight,
+}: ShadowsStyleOptions) {
+  return css`
+    position: absolute;
+    z-index: ${tokens.zIndex.default};
+    top: ${HEAD_CELL_HEIGHT}px;
+    left: 0;
+    height: calc(100% - ${HEAD_CELL_HEIGHT}px);
+    width: 100%;
+    pointer-events: none;
+    ${hasShadowLeft &&
+    `border-left: ${tokens.borderWidth.thin} solid ${hexToRgba(
+      tokens.colors.navy,
+      0.15,
+    )};`}
+    ${hasShadowRight &&
+    `border-right: ${tokens.borderWidth.thin} solid ${hexToRgba(
+      tokens.colors.navy,
+      0.15,
+    )};`}
+    box-shadow: ${hasShadowLeft &&
+    'inset 12px 0 12px -12px rgba(5, 25, 45, 0.3)'}${hasShadowLeft &&
+    hasShadowRight &&
+    ','}${hasShadowRight && 'inset -12px 0 12px -12px rgba(5, 25, 45, 0.3)'};
+  `;
+}
+
+type TableStyleOptions = WithShadows;
+
+// Basic table styles with round corners applied to corner cells
+// When shadows are visible corners are not rounded
+export function tableStyle({
+  hasShadowLeft,
+  hasShadowRight,
+}: TableStyleOptions) {
   return css`
     border: 0;
     border-collapse: separate;
     border-spacing: 0;
     margin: 0;
+
+    ${!hasShadowLeft &&
+    css`
+      tr:first-of-type td:first-of-type {
+        border-top-left-radius: ${tokens.borderRadius.medium};
+      }
+
+      tr:last-of-type td:first-of-type {
+        border-bottom-left-radius: ${tokens.borderRadius.medium};
+      }
+    `}
+
+    ${!hasShadowRight &&
+    css`
+      tr:first-of-type td:last-of-type {
+        border-top-right-radius: ${tokens.borderRadius.medium};
+      }
+
+      tr:last-of-type td:last-of-type {
+        border-bottom-right-radius: ${tokens.borderRadius.medium};
+      }
+    `}
   `;
 }
 
@@ -39,13 +128,14 @@ const cellBaseStyle = css`
 export function headCellStyle() {
   return css`
     ${cellBaseStyle}
+    height: ${HEAD_CELL_HEIGHT}px;
     font-size: ${tokens.fontSizes.small};
     font-weight: ${tokens.fontWeights.bold};
     line-height: ${tokens.lineHeights.default};
     letter-spacing: ${tokens.letterSpacing.relaxed};
     text-transform: uppercase;
     border-top: 0;
-    min-width: 120px;
+    min-width: 200px;
     max-width: 90vw;
   `;
 }
@@ -69,22 +159,6 @@ export function cellStyle() {
     &:last-of-type {
       border-right: ${tokens.borderWidth.thin} solid
         ${hexToRgba(tokens.colors.navy, 0.15)};
-    }
-
-    tr:first-of-type &:first-of-type {
-      border-top-left-radius: ${tokens.borderRadius.medium};
-    }
-
-    tr:first-of-type &:last-of-type {
-      border-top-right-radius: ${tokens.borderRadius.medium};
-    }
-
-    tr:last-of-type &:first-of-type {
-      border-bottom-left-radius: ${tokens.borderRadius.medium};
-    }
-
-    tr:last-of-type &:last-of-type {
-      border-bottom-right-radius: ${tokens.borderRadius.medium};
     }
 
     tr:last-of-type & {
--- src/table/table-internal.tsx
+++ src/table/table-internal.tsx
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useFocusRing } from '@react-aria/focus';
 
-import { tableWrapperStyle, tableStyle } from './styles';
+import useScrollPosition from './use-scroll-position';
+import {
+  outerWrapperStyle,
+  tableWrapperStyle,
+  tableStyle,
+  shadowsStyle,
+} from './styles';
 
 type TableProps = React.HTMLAttributes<HTMLTableElement>;
 
 function TableInternal(props: TableProps) {
   const { focusProps, isFocusVisible } = useFocusRing();
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const { isAtLeft, isAtRight } = useScrollPosition(wrapperRef);
+  const hasShadowLeft = !isAtLeft;
+  const hasShadowRight = !isAtRight;
 
   return (
-    <div
-      {...focusProps}
-      role="region"
-      aria-label="Some table"
-      tabIndex={0}
-      css={tableWrapperStyle({ isFocusVisible })}
-    >
-      <table {...props} css={tableStyle()} />
+    <div css={outerWrapperStyle()}>
+      <div
+        css={shadowsStyle({
+          hasShadowLeft,
+          hasShadowRight,
+        })}
+      />
+      <div
+        {...focusProps}
+        ref={wrapperRef}
+        role="region"
+        aria-label="Some table"
+        tabIndex={0}
+        css={tableWrapperStyle({
+          isFocusVisible,
+          hasShadowLeft,
+          hasShadowRight,
+        })}
+      >
+        <table {...props} css={tableStyle({ hasShadowLeft, hasShadowRight })} />
+      </div>
     </div>
   );
 }
