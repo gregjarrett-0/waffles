e5f809eec4aaa2c151fef93f496d5bdb86b859dc
blurbyte <info@blurbyte.com>
2022-10-11 11:16:48 +0200

    docs(card): update basic example

--- doc-site/examples/card/basic-usage.tsx
+++ doc-site/examples/card/basic-usage.tsx
@@ -1,20 +1,17 @@
-import { css } from '@emotion/react';
 import { Paragraph } from '@datacamp/waffles/paragraph';
 import { Heading } from '@datacamp/waffles/heading';
 import { Card } from '@datacamp/waffles/card';
+import { Button } from '@datacamp/waffles/button';
 
 function Example() {
   return (
     <Card>
       <Heading size="large">Intermediate Python</Heading>
-      <Paragraph
-        css={css`
-          margin-bottom: 0;
-        `}
-      >
+      <Paragraph>
         Level up your data science skills by creating visualizations using
         Matplotlib and manipulating DataFrames with pandas.
       </Paragraph>
+      <Button>Check course</Button>
     </Card>
   );
 }
