7ba498c3f90844c0d3f1049fbe28304a9ebf11aa
ixTec <christian.dalby@datacamp.com>
2022-10-11 12:11:29 +0100

    docs(notification): add banner example

---
+++ doc-site/examples/notification/banner.tsx
@@ -0,0 +1,22 @@
+import { Notification } from '@datacamp/waffles/notification';
+import { Link } from '@datacamp/waffles/link';
+
+function Example() {
+  return (
+    <Notification
+      title="Course updated"
+      description={
+        <>
+          The{' '}
+          <Link href="https://app.datacamp.com/learn/courses/reporting-in-sql">
+            Reporting in SQL
+          </Link>{' '}
+          course has been updated recently.
+        </>
+      }
+      mode="banner"
+    />
+  );
+}
+
+export default Example;
