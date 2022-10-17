e41af052cf842ee1fe379ba5ebf315ef580f8ae1
ixTec <christian.dalby@datacamp.com>
2022-10-17 15:34:38 +0100

    test(brand): update tests and snapshots

--- src/brand/__tests__/brand.e2e.ts
+++ src/brand/__tests__/brand.e2e.ts
@@ -1,21 +1,21 @@
 describe('Brand components', () => {
   it('render all available Brand components', () => {
     cy.loadStory('brand-variants');
-    cy.findAllByTestId('brand-row').should('have.length', 4);
+    cy.findAllByTestId('brand-row').should('have.length', 16);
   });
 
   it('render all available Brand components as inverted', () => {
     cy.loadStory('brand-inverted');
-    cy.findAllByTestId('brand-row').should('have.length', 4);
+    cy.findAllByTestId('brand-row').should('have.length', 16);
   });
 
   it('render all available Brand components as monochrome', () => {
     cy.loadStory('brand-monochrome');
-    cy.findAllByTestId('brand-row').should('have.length', 4);
+    cy.findAllByTestId('brand-row').should('have.length', 16);
   });
 
   it('render all available Brand components as monochrome and inverted', () => {
     cy.loadStory('brand-monochrome-inverted');
-    cy.findAllByTestId('brand-row').should('have.length', 4);
+    cy.findAllByTestId('brand-row').should('have.length', 16);
   });
 });
