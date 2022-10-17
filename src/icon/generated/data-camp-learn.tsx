97f53d11b6531e2a3aa7e6f17018822f4c8f24da
ixTec <christian.dalby@datacamp.com>
2022-10-17 16:34:55 +0100

    feat(icon): add new product icons

---
+++ src/icon/generated/data-camp-learn.tsx
@@ -0,0 +1,18 @@
+// AUTO-GENERATED CONTENT - DO NOT MANUALLY EDIT - Run 'yarn generate:icons' to update
+
+import Icon from '../icon-internal';
+
+type DataCampLearnProps = Omit<React.ComponentProps<typeof Icon>, 'children'>;
+
+function DataCampLearn({ size, ...restProps }: DataCampLearnProps) {
+  return (
+    <Icon viewBox="0 0 18 18" size={size} {...restProps}>
+      <path
+        fill="currentColor"
+        d="M8.743 1.249.58 6.072a1.18 1.18 0 0 0-.005 2.029l2.343 1.401.009 2.901c0 .411.217.793.569 1.005l5.245 3.173c.375.227.847.227 1.222 0l3.931-2.38v-1.654l-4.542 2.751-5.009-3.032-.007-1.915 4.409 2.635c.373.224.84.222 1.213 0l7.469-4.483c.76-.458.764-1.557.007-2.02l-5.41-3.291h-.004L3.626 8.275 1.644 7.088l8.486-5.013h.002m1.89 2.774 4.341 2.64-7.012 4.209-4.35-2.602 7.021-4.247z"
+      />
+    </Icon>
+  );
+}
+
+export default DataCampLearn;
