1bc850460d94fd408c61dc6d7194725cf2bd60fe
ixTec <christian.dalby@datacamp.com>
2022-11-04 10:11:49 +0000

    test(skill-tag): split stories

--- src/skill-tag/__tests__/skill-tag.e2e.ts
+++ src/skill-tag/__tests__/skill-tag.e2e.ts
@@ -1,6 +1,11 @@
 describe('Skill Tag', () => {
-  it('render all levels in both level and in inverted and regular', () => {
-    cy.loadStory('skill-tag-variants-and-levels');
-    cy.get('main').findAllByTestId('skill-tag').should('have.length', 12);
+  it('render all variants and levels', () => {
+    cy.loadStory('skill-tag-regular');
+    cy.get('main').findAllByTestId('skill-tag').should('have.length', 6);
+  });
+
+  it('render all variants and levels as inverted', () => {
+    cy.loadStory('skill-tag-inverted');
+    cy.get('main').findAllByTestId('skill-tag').should('have.length', 6);
   });
 });
