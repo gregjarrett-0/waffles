ac64ad10e10564e90dd568dd07e8ddcb8d6d82b4
ixTec <christian.dalby@datacamp.com>
2022-11-04 14:04:56 +0000

    feat(asset): add new alpa icon assets

---
+++ src/asset/generated/learn-alpa.tsx
@@ -0,0 +1,20 @@
+// AUTO-GENERATED CONTENT - DO NOT MANUALLY EDIT - Run 'yarn generate:assets' to update
+
+import Asset from '../asset-internal';
+
+type LearnAlpaProps = Omit<React.ComponentProps<typeof Asset>, 'children'>;
+
+function LearnAlpa({ ...restProps }: LearnAlpaProps) {
+  return (
+    <Asset viewBox="0 0 40 40" {...restProps}>
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M5.171 14.783C5.171 6.617 11.812 0 20 0s14.829 6.617 14.829 14.783a14.76 14.76 0 0 1-7.109 12.623l-.097.058v6.097H12.406v-6.079l-.036-.021a14.768 14.768 0 0 1-7.198-12.49l-.001-.188zm25.756 0c0-6.007-4.89-10.88-10.927-10.88-6.036 0-10.927 4.873-10.927 10.88 0 4.201 2.412 7.971 6.138 9.783l1.098.534v4.559h7.411v-4.57l1.093-.535a10.864 10.864 0 0 0 6.114-9.771zm-4.526 21.315L24.148 40h-8.295L13.6 36.098h12.801zM20.482 8.856h-4.031l-2.107 8.125h5.545l-1.094 4.602h4.011l2.022-8.505h-5.441l1.095-4.222z"
+      />
+    </Asset>
+  );
+}
+
+export default LearnAlpa;
---
+++ src/asset/generated/practice-alpa.tsx
@@ -0,0 +1,20 @@
+// AUTO-GENERATED CONTENT - DO NOT MANUALLY EDIT - Run 'yarn generate:assets' to update
+
+import Asset from '../asset-internal';
+
+type PracticeAlpaProps = Omit<React.ComponentProps<typeof Asset>, 'children'>;
+
+function PracticeAlpa({ ...restProps }: PracticeAlpaProps) {
+  return (
+    <Asset viewBox="0 0 40 40" {...restProps}>
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="m33.602 8.931.017 22.131-4.256.003-.008-8.939H10.647l.007 8.939-4.256.003-.017-22.13 4.256-.003.005 8.937h18.709l-.005-8.937 4.256-.004zm2.129 3.22 4.258 2.458L40 24.96l-4.251 2.453-.018-15.262zm-31.48.256.018 14.769-4.258-2.458L0 14.861l4.251-2.454z"
+      />
+    </Asset>
+  );
+}
+
+export default PracticeAlpa;
