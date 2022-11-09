10527684c87d551a82f858c5ad78f737b1c24420
ixTec <christian.dalby@datacamp.com>
2022-11-09 13:38:41 +0000

    test(brand): update tests

--- src/icon/__tests__/icon.e2e.ts
+++ src/icon/__tests__/icon.e2e.ts
@@ -1,6 +1,6 @@
 describe('Icon', () => {
   it('render all available icons in different sizes', () => {
     cy.loadStory('icon-sizes');
-    cy.findAllByTestId('icon-row').should('have.length', 220);
+    cy.findAllByTestId('icon-row').should('have.length', 221);
   });
 });
