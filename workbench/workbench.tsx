c216cab6f97111c819f876efd6ada7449e1048e2
ixTec <christian.dalby@datacamp.com>
2022-11-22 17:04:54 +0000

    test(notification): update default tests and snapshots

--- workbench/workbench.tsx
+++ workbench/workbench.tsx
@@ -1,10 +1,147 @@
+import { useState } from 'react';
+import { css } from '@emotion/react';
+import { tokens } from '@datacamp/waffles/tokens';
+import { Paragraph } from '@datacamp/waffles/paragraph';
+import { Drawer } from '@datacamp/waffles/drawer';
+import { Dialog } from '@datacamp/waffles/dialog';
+import { Button } from '@datacamp/waffles/button';
+import { AlertDialog } from '@datacamp/waffles/alert-dialog';
+
+const mainContent = css`
+  padding: ${tokens.spacing.medium};
+`;
+
 function Workbench() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isAlertOpen, setIsAlertOpen] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [isRightDrawerOpen, setIsRightDrawerOpen] = useState(false);
+
   return (
     <>
-      {/*
-        Develop and test your components here.
-        IMPORTANT! Please do not commit your work to this file so it remains pristine for other developers.
-      */}
+      <article css={mainContent}>
+        <Button onClick={() => setIsAlertOpen(true)}>Open Alert</Button>
+        <AlertDialog isOpen={isAlertOpen} onClose={() => setIsAlertOpen(false)}>
+          <AlertDialog.Header>Title of a Dialog</AlertDialog.Header>
+          <AlertDialog.Body>
+            <Paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Paragraph>
+          </AlertDialog.Body>
+          <AlertDialog.Footer>
+            <AlertDialog.Button
+              variant="secondary"
+              onClick={() => setIsAlertOpen(false)}
+            >
+              Cancel
+            </AlertDialog.Button>
+            <AlertDialog.Button onClick={() => setIsAlertOpen(false)}>
+              Confirm
+            </AlertDialog.Button>
+          </AlertDialog.Footer>
+        </AlertDialog>
+      </article>
+      <article css={mainContent}>
+        <Button onClick={() => setIsDialogOpen(true)}>Open Dialog</Button>
+        <Dialog
+          isOpen={isDialogOpen}
+          onClose={() => {
+            setIsDialogOpen(false);
+          }}
+        >
+          <Dialog.Header>Title of a Dialog</Dialog.Header>
+          <Dialog.Body>
+            <Paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Paragraph>
+          </Dialog.Body>
+          <Dialog.Footer>
+            <Dialog.Button
+              variant="secondary"
+              onClick={() => setIsDialogOpen(false)}
+            >
+              Cancel
+            </Dialog.Button>
+            <Dialog.Button autoFocus onClick={() => setIsDialogOpen(false)}>
+              Confirm
+            </Dialog.Button>
+          </Dialog.Footer>
+        </Dialog>
+      </article>
+      <article css={mainContent}>
+        <Button onClick={() => setIsDrawerOpen(true)}>Open Drawer Left</Button>
+        <Drawer
+          placement="left"
+          isOpen={isDrawerOpen}
+          onClose={() => setIsDrawerOpen(false)}
+        >
+          <Drawer.Header>Drawer Title</Drawer.Header>
+          <Drawer.Body>
+            <Paragraph>
+              Main content of the drawer. Even very long content is supported.
+            </Paragraph>
+          </Drawer.Body>
+          <Drawer.Footer>
+            <Drawer.Button
+              variant="secondary"
+              onClick={() => setIsDrawerOpen(false)}
+            >
+              Cancel
+            </Drawer.Button>
+            <Drawer.Button
+              autoFocus
+              variant="primary"
+              onClick={() => setIsDrawerOpen(false)}
+            >
+              Confirm
+            </Drawer.Button>
+          </Drawer.Footer>
+        </Drawer>
+      </article>
+      <article css={mainContent}>
+        <Button onClick={() => setIsRightDrawerOpen(true)}>
+          Open Drawer Right
+        </Button>
+        <Drawer
+          placement="right"
+          isOpen={isRightDrawerOpen}
+          onClose={() => setIsRightDrawerOpen(false)}
+        >
+          <Drawer.Header>Drawer Title</Drawer.Header>
+          <Drawer.Body>
+            <Paragraph>
+              Main content of the drawer. Even very long content is supported.
+            </Paragraph>
+          </Drawer.Body>
+          <Drawer.Footer>
+            <Drawer.Button
+              variant="secondary"
+              onClick={() => setIsRightDrawerOpen(false)}
+            >
+              Cancel
+            </Drawer.Button>
+            <Drawer.Button
+              autoFocus
+              variant="primary"
+              onClick={() => setIsRightDrawerOpen(false)}
+            >
+              Confirm
+            </Drawer.Button>
+          </Drawer.Footer>
+        </Drawer>
+      </article>
     </>
   );
 }
