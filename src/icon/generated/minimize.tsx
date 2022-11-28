c96fb94d3f772a880531861f3fac04bd130fb874
blurbyte <info@blurbyte.com>
2022-11-28 09:49:42 +0100

    feat(icon): add Minimize icon

---
+++ src/icon/generated/minimize.tsx
@@ -0,0 +1,20 @@
+// AUTO-GENERATED CONTENT - DO NOT MANUALLY EDIT - Run 'yarn generate:icons' to update
+
+import Icon from '../icon-internal';
+
+type MinimizeProps = Omit<React.ComponentProps<typeof Icon>, 'children'>;
+
+function Minimize({ size, ...restProps }: MinimizeProps) {
+  return (
+    <Icon viewBox="0 0 18 18" size={size} {...restProps}>
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M16 8a1 1 0 0 1-1 1h-4.5A1.5 1.5 0 0 1 9 7.5V3a1 1 0 0 1 2 0v2.589l4.29-4.29a.999.999 0 1 1 1.414 1.414L12.418 7H15a1 1 0 0 1 1 1zM2 10a1 1 0 0 1 1-1h4.5A1.5 1.5 0 0 1 9 10.5V15a1 1 0 0 1-2 0v-2.589l-4.29 4.29a1 1 0 0 1-1.414-1.414L5.582 11H3a1 1 0 0 1-1-1z"
+      />
+    </Icon>
+  );
+}
+
+export default Minimize;
