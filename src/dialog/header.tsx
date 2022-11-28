4d3e457b937bd5fb6b1a722798658912ede50425
ixTec <christian.dalby@datacamp.com>
2022-11-28 15:43:46 +0000

    feat(dialog): add support for custom icon override

--- src/dialog/header.tsx
+++ src/dialog/header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { cloneElement } from 'react';
 
 import { NotificationIcon } from '../notification-icon';
 import { Heading } from '../heading';
@@ -10,9 +10,11 @@ type HeaderProps = {
   /* Mode of the dialog. */
   /* @default plain */
   mode?: 'plain' | 'decorative';
-  /* Variant of the dialog, when `mode="decorative"`. */
+  /* Variant of the dialog, only applicable when `mode="decorative"`. */
   /* @default info */
   variant?: 'info' | 'success' | 'warning' | 'error' | 'upgrade';
+  /* Custom icon override for the decorative header, only applicable when `mode="decorative"`. In general, use `large` Waffles [Icon](/components/icon). */
+  customIconOverride?: JSX.Element;
   /* Title of the dialog. */
   children: React.ReactNode;
 } & React.HTMLAttributes<HTMLHeadingElement>;
@@ -20,13 +22,19 @@ type HeaderProps = {
 function Header({
   mode = 'plain',
   variant = 'info',
+  customIconOverride,
   children,
   ...restProps
 }: HeaderProps) {
   const { headerId } = useDialog();
 
+  // Auto-set size
   function renderIcon() {
-    return <NotificationIcon variant={variant} size="large" />;
+    return customIconOverride ? (
+      cloneElement(customIconOverride, { size: 'large' })
+    ) : (
+      <NotificationIcon variant={variant} size="large" />
+    );
   }
 
   return (
