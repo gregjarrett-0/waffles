32257cd1a06d6e8928e0c8b98b1e4a2a5558876f
blurbyte <info@blurbyte.com>
2022-11-28 10:51:56 +0100

    docs: update Design Principles page

--- doc-site/components/design-principles-card.tsx
+++ doc-site/components/design-principles-card.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { css } from '@emotion/react';
 import { tokens } from '@datacamp/waffles/tokens';
+import { mediaQuery } from '@datacamp/waffles/helpers';
 import { Heading } from '@datacamp/waffles/heading';
 import { Card } from '@datacamp/waffles/card';
 
@@ -15,19 +16,42 @@ const cardStyle = css`
 const headingStyle = css`
   text-align: center;
   margin: ${tokens.spacing.medium} 0 ${tokens.spacing.small};
+  max-width: 600px;
 `;
 
 type BestPracticesProps = {
   title: string;
   icon: React.ReactNode;
-  children: React.ReactNode;
+  children?: React.ReactNode;
+  isExpended?: boolean;
 };
 
-function DesignPrinciplesCard({ title, icon, children }: BestPracticesProps) {
+function DesignPrinciplesCard({
+  title,
+  icon,
+  children,
+  isExpended = false,
+}: BestPracticesProps) {
   return (
-    <Card css={cardStyle}>
+    <Card
+      css={css`
+        ${cardStyle}
+        ${isExpended &&
+        css`
+          ${mediaQuery.aboveSmall} {
+            grid-column: span 2;
+          }
+        `}
+      `}
+    >
       {icon}
-      <Heading css={headingStyle}>{title}</Heading>
+      <Heading
+        as="h2"
+        size={isExpended ? 'xxlarge' : 'large'}
+        css={headingStyle}
+      >
+        {title}
+      </Heading>
       {children}
     </Card>
   );
