ce69c8e501ce162c6941ec7be686937d9b20d050
ixTec <christian.dalby@datacamp.com>
2022-12-02 14:58:59 +0000

    test(dialog): update decorative header variants story

--- src/dialog/__tests__/dialog-decorative-header-variants.story.tsx
+++ src/dialog/__tests__/dialog-decorative-header-variants.story.tsx
@@ -6,151 +6,194 @@ import { tokens } from '../../tokens';
 import { Paragraph } from '../../paragraph';
 import { Button } from '../../button';
 
-const mainContent = css`
-  padding: ${tokens.spacing.medium};
+const exampleWrapper = css`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  gap: ${tokens.spacing.small};
 `;
 
+type DialogVariants = 'info' | 'success' | 'warning' | 'error' | 'upgrade';
+
 function Story() {
-  const [isOpen, setIsOpen] = useState([false, false, false, false, false]);
+  const [openDialog, setOpenDialog] = useState<DialogVariants | null>();
 
   return (
-    <>
-      <article css={mainContent}>
-        <Button
-          onClick={() => setIsOpen([true, false, false, false, false])}
-        >{`Open Info Dialog`}</Button>
-        <Dialog
-          isOpen={isOpen[0]}
-          onClose={() => setIsOpen([false, false, false, false, false])}
-        >
-          <Dialog.Header mode="decorative" variant="info">
-            Title of a Dialog
-          </Dialog.Header>
-          <Dialog.Body>
-            <Paragraph>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </Paragraph>
-          </Dialog.Body>
-          <Dialog.Footer>
-            <Dialog.Button
-              autoFocus
-              onClick={() => setIsOpen([false, false, false, false, false])}
-            >
-              Dismiss
-            </Dialog.Button>
-          </Dialog.Footer>
-        </Dialog>
-      </article>
-      <article css={mainContent}>
-        <Button
-          onClick={() => setIsOpen([false, true, false, false, false])}
-        >{`Open Success Dialog`}</Button>
-        <Dialog
-          isOpen={isOpen[1]}
-          onClose={() => setIsOpen([false, false, false, false, false])}
-        >
-          <Dialog.Header mode="decorative" variant="success">
-            Title of a Dialog
-          </Dialog.Header>
-          <Dialog.Body>
-            <Paragraph>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </Paragraph>
-          </Dialog.Body>
-          <Dialog.Footer>
-            <Dialog.Button
-              autoFocus
-              onClick={() => setIsOpen([false, false, false, false, false])}
-            >
-              Dismiss
-            </Dialog.Button>
-          </Dialog.Footer>
-        </Dialog>
-      </article>
-      <article css={mainContent}>
-        <Button
-          onClick={() => setIsOpen([false, false, true, false, false])}
-        >{`Open Warning Dialog`}</Button>
-        <Dialog
-          isOpen={isOpen[2]}
-          onClose={() => setIsOpen([false, false, false, false, false])}
-        >
-          <Dialog.Header mode="decorative" variant="warning">
-            Title of a Dialog
-          </Dialog.Header>
-          <Dialog.Body>
-            <Paragraph>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </Paragraph>
-          </Dialog.Body>
-          <Dialog.Footer>
-            <Dialog.Button
-              autoFocus
-              onClick={() => setIsOpen([false, false, false, false, false])}
-            >
-              Dismiss
-            </Dialog.Button>
-          </Dialog.Footer>
-        </Dialog>
-      </article>
-      <article css={mainContent}>
-        <Button
-          onClick={() => setIsOpen([false, false, false, true, false])}
-        >{`Open Error Dialog`}</Button>
-        <Dialog
-          isOpen={isOpen[3]}
-          onClose={() => setIsOpen([false, false, false, false, false])}
-        >
-          <Dialog.Header mode="decorative" variant="error">
-            Title of a Dialog
-          </Dialog.Header>
-          <Dialog.Body>
-            <Paragraph>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </Paragraph>
-          </Dialog.Body>
-          <Dialog.Footer>
-            <Dialog.Button
-              autoFocus
-              onClick={() => setIsOpen([false, false, false, false, false])}
-            >
-              Dismiss
-            </Dialog.Button>
-          </Dialog.Footer>
-        </Dialog>
-      </article>
-      <article css={mainContent}>
-        <Button
-          onClick={() => setIsOpen([false, false, false, false, true])}
-        >{`Open Upgrade Dialog`}</Button>
-        <Dialog
-          isOpen={isOpen[4]}
-          onClose={() => setIsOpen([false, false, false, false, false])}
-        >
-          <Dialog.Header mode="decorative" variant="upgrade">
-            Title of a Dialog
-          </Dialog.Header>
-          <Dialog.Body>
-            <Paragraph>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </Paragraph>
-          </Dialog.Body>
-          <Dialog.Footer>
-            <Dialog.Button
-              autoFocus
-              onClick={() => setIsOpen([false, false, false, false, false])}
-            >
-              Dismiss
-            </Dialog.Button>
-          </Dialog.Footer>
-        </Dialog>
-      </article>
-    </>
+    <div css={exampleWrapper}>
+      {/* Info Dialog */}
+      <Button variant="secondary" onClick={() => setOpenDialog('info')}>
+        Open Info Dialog
+      </Button>
+      <Dialog
+        isOpen={openDialog === 'info'}
+        onClose={() => setOpenDialog(null)}
+      >
+        <Dialog.Header mode="decorative" variant="info">
+          Confirm your case study participation
+        </Dialog.Header>
+        <Dialog.Body>
+          <Paragraph>
+            To be able to participate in the case study, you will need a
+            computer with a webcam. If you fail to submit, you will have to wait
+            4 weeks before taking the case study again.
+          </Paragraph>
+        </Dialog.Body>
+        <Dialog.Footer>
+          <Dialog.Button
+            variant="secondary"
+            onClick={() => setOpenDialog(null)}
+          >
+            Cancel
+          </Dialog.Button>
+          <Dialog.Button
+            autoFocus
+            variant="primary"
+            onClick={() => setOpenDialog(null)}
+          >
+            Confirm
+          </Dialog.Button>
+        </Dialog.Footer>
+      </Dialog>
+
+      {/* Success Dialog */}
+      <Button variant="secondary" onClick={() => setOpenDialog('success')}>
+        Open Success Dialog
+      </Button>
+      <Dialog
+        isOpen={openDialog === 'success'}
+        onClose={() => setOpenDialog(null)}
+      >
+        <Dialog.Header mode="decorative" variant="success">
+          Confirm your case study participation
+        </Dialog.Header>
+        <Dialog.Body>
+          <Paragraph>
+            To be able to participate in the case study, you will need a
+            computer with a webcam. If you fail to submit, you will have to wait
+            4 weeks before taking the case study again.
+          </Paragraph>
+        </Dialog.Body>
+        <Dialog.Footer>
+          <Dialog.Button
+            variant="secondary"
+            onClick={() => setOpenDialog(null)}
+          >
+            Cancel
+          </Dialog.Button>
+          <Dialog.Button
+            autoFocus
+            variant="primary"
+            onClick={() => setOpenDialog(null)}
+          >
+            Confirm
+          </Dialog.Button>
+        </Dialog.Footer>
+      </Dialog>
+
+      {/* Warning Dialog */}
+      <Button variant="secondary" onClick={() => setOpenDialog('warning')}>
+        Open Warning Dialog
+      </Button>
+      <Dialog
+        isOpen={openDialog === 'warning'}
+        onClose={() => setOpenDialog(null)}
+      >
+        <Dialog.Header mode="decorative" variant="warning">
+          Confirm your case study participation
+        </Dialog.Header>
+        <Dialog.Body>
+          <Paragraph>
+            To be able to participate in the case study, you will need a
+            computer with a webcam. If you fail to submit, you will have to wait
+            4 weeks before taking the case study again.
+          </Paragraph>
+        </Dialog.Body>
+        <Dialog.Footer>
+          <Dialog.Button
+            variant="secondary"
+            onClick={() => setOpenDialog(null)}
+          >
+            Cancel
+          </Dialog.Button>
+          <Dialog.Button
+            autoFocus
+            variant="primary"
+            onClick={() => setOpenDialog(null)}
+          >
+            Confirm
+          </Dialog.Button>
+        </Dialog.Footer>
+      </Dialog>
+
+      {/* Error Dialog */}
+      <Button variant="secondary" onClick={() => setOpenDialog('error')}>
+        Open Error Dialog
+      </Button>
+      <Dialog
+        isOpen={openDialog === 'error'}
+        onClose={() => setOpenDialog(null)}
+      >
+        <Dialog.Header mode="decorative" variant="error">
+          Confirm your case study participation
+        </Dialog.Header>
+        <Dialog.Body>
+          <Paragraph>
+            To be able to participate in the case study, you will need a
+            computer with a webcam. If you fail to submit, you will have to wait
+            4 weeks before taking the case study again.
+          </Paragraph>
+        </Dialog.Body>
+        <Dialog.Footer>
+          <Dialog.Button
+            variant="secondary"
+            onClick={() => setOpenDialog(null)}
+          >
+            Cancel
+          </Dialog.Button>
+          <Dialog.Button
+            autoFocus
+            variant="primary"
+            onClick={() => setOpenDialog(null)}
+          >
+            Confirm
+          </Dialog.Button>
+        </Dialog.Footer>
+      </Dialog>
+
+      {/* Upgrade Dialog */}
+      <Button variant="secondary" onClick={() => setOpenDialog('upgrade')}>
+        Open Upgrade Dialog
+      </Button>
+      <Dialog
+        isOpen={openDialog === 'upgrade'}
+        onClose={() => setOpenDialog(null)}
+      >
+        <Dialog.Header mode="decorative" variant="upgrade">
+          Confirm your case study participation
+        </Dialog.Header>
+        <Dialog.Body>
+          <Paragraph>
+            To be able to participate in the case study, you will need a
+            computer with a webcam. If you fail to submit, you will have to wait
+            4 weeks before taking the case study again.
+          </Paragraph>
+        </Dialog.Body>
+        <Dialog.Footer>
+          <Dialog.Button
+            variant="secondary"
+            onClick={() => setOpenDialog(null)}
+          >
+            Cancel
+          </Dialog.Button>
+          <Dialog.Button
+            autoFocus
+            variant="primary"
+            onClick={() => setOpenDialog(null)}
+          >
+            Confirm
+          </Dialog.Button>
+        </Dialog.Footer>
+      </Dialog>
+    </div>
   );
 }
 
